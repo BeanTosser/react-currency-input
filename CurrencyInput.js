266dc020e912217246fc3c270501611ae1c9347b
timetherewere <mikjosjon@gmail.com>
2020-06-27 19:41:33 -0400

    Basic currency input functioning (with some hacky formatting workarounds in the css).

---
+++ CurrencyInput.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './CurrencyInput.css';
+
+class CurrencyInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.formattedValue = this.formatToDecimal(this.currencyValue);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  formatToDecimal(num){
+    return parseFloat(num).toFixed(parseFloat(this.props.decimalPlaces));
+  }
+
+  onChange(event){
+    let formattedValue = parseFloat(event.target.value).toFixed(this.props.decimalPlaces);
+    this.props.onChange(event, formattedValue);
+  }
+
+  render() {
+    return(
+      <span className={"CurrencyInput " + this.props.className}>
+        {this.props.currencySymbol}<input value = {this.formatToDecimal(this.props.value)} type="number" name={this.props.name} size={this.props.size} onChange={this.onChange}/>
+      </span>
+    );
+  }
+}
+
+export default CurrencyInput;
